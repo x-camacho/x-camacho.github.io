1148c8abb8521972b73015c80ffbfee6a8c34e4e
x-camacho <xac923@outlook.com>
2021-11-26 19:00:42 -0800

    Commit #5 - included will2live timer

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
+//////* GRAB USER NAME FROM URL, STORE AND PUSH TO GAME PAGE */////
+let userName = [];
+location.search.substr(1).split("&").forEach(function(item) {userName[item.split("=")[0]] = item.split("=")[1]});
+console.log(userName)
+document.getElementById('plantName1').innerText = userName.plantName;
+//////*  END GRAB USER NAME FROM URL, STORE AND PUSH TO GAME PAGE */////
+
+
 //////* BUTTON FUNCTION VARIABLES && MECHANICS */////
 let level = document.getElementById('level');
 let levelCount = 1;
@@ -9,15 +17,6 @@ let counter3 = document.getElementById("counter3");
 let count1 = 0;
 let count2 = 0;
 let count3 = 0;
-//////* END */////
-
-//////* GRAB USER NAME FROM URL, STORE AND PUSH TO GAME PAGE */////
-let userName = [];
-location.search.substr(1).split("&").forEach(function(item) {userName[item.split("=")[0]] = item.split("=")[1]});
-console.log(userName)
-document.getElementById('plantName1').innerText = userName.plantName;
-//////* END */////
-
 
 document.getElementById("plus").addEventListener("click", function() {
     let newValue = parseInt(progress.getAttribute('data-value')) + 1;
@@ -72,31 +71,17 @@ function reset() {
      count3 = 0;
      counter3.innerText = 0 + " / 5";
      levelCount = levelCount + 1;
-     level.innerText = "Level: " + levelCount;
+     level.innerText = "Level:" + levelCount;
      plus.disabled = false;
      plus.innerText = "Sun!"
      plus2.disabled = false;
      plus2.innerText = "Water!";
      plus3.disabled = false;
      plus3.innerText = "Love";
+     willLeft += 20;
 }
-//////* BUTTON FUNCTION VARIABLES && MECHANICS END *//////
+//////* END BUTTON FUNCTION VARIABLES && MECHANICS END *//////
 
-///* PSUEDO CODE ///*
-// increase counter after every button //
-// diable button click at 10 clicks for 5 seconds //
-
-//////* Timer that Runs Down *//////
-// const button = document.querySelector("#restart-button");
-const bars = document.querySelectorAll(".round-time-bar");
-button.addEventListener("click", () => {
-  bars.forEach((bar) => {
-    bar.classList.remove("round-time-bar");
-    bar.offsetWidth;
-    bar.classList.add("round-time-bar");
-  });
-});
-//////* END */////
 
 //////* DISABLE BUTTON && INITIALIZE COOLDOWN*//////
 function coolDown() {
@@ -131,5 +116,34 @@ function coolDown3() {
       counter3.innerText = count3 + " / 5";
       }, 20000);
   }
-//////* END */////
+//////* END DISABLE BUTTON && INITIALIZE COOLDOWN*//////
   
+
+///* Will Meter *///
+function willtoLive() {
+let willLeft = 10;
+let interval = setInterval(function() {
+    document.getElementById('timer').innerHTML = --willLeft + " seconds";
+    if (willLeft <= 0) {
+        plus.disabled = true;
+        plus2.disabled = true;
+        plus3.disabled = true;
+       document.getElementById('timer').innerHTML = "Oh, no! " + userName.plantName + " has lost the will to live!" + " Continue?";
+       let a = document.createElement('a');
+       let a2 = document.createElement('a');
+       let contentYes = document.createTextNode("Yes!");
+       let contentNo = document.createTextNode("No!")
+       a.appendChild(contentYes);
+       a2.appendChild(contentNo);
+       a.href = "name_your_plant.html";
+        a2.href = "welcome.html";
+       document.getElementById('continueYes').appendChild(a);
+        document.getElementById('continueNo').appendChild(a2);
+       clearInterval(interval);
+    }
+}, 1000);
+}
+
+willtoLive();
+
+    //    window.alert("Would you like to try again?");
